c42c6e71facdabce479b2ea2a4cc13d8476ae607
c3026923 <Jack.Watson3@student.shu.ac.uk>
2024-10-09 12:43:48 +0100

    Initial dashboard.

---
+++ dashboard.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="css/style.css">
+    
+</head>
+<body>
+<h1>KPIs</h1>
+   <div class="grid-container">
+        <div class="grid-element">
+            <p>Inventory Turnover Rate</p>
+                        <p>5</p>
+        </div>
+        <div class="grid-element">
+            <p>Product Sales</p>
+                        <p><b>£7,500</b></p>
+        </div>
+                <div class="grid-element">
+            <p>Rate of Return</p>
+                        <p><b>7%</b></p>
+        </div>
+                <div class="grid-element">
+            <p>Average Inventory</p>
+            <p>£0000</p>
+        </div>
+   </div>
+   <div class="button-area">
+   <button>View Analytics</button>
+      <button class="green">Add Products</button>
+
+   </div>
+
+   
+</body>
+</html> 
