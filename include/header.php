9fc656810fc05cc8edac3e23a458654c86236bbb
Connor2308 <c0027666@hallam.shu.ac.uk>
2024-10-09 12:03:16 +0100

    faq

---
+++ include/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>#########</title>
+    <link rel="stylesheet" href="css/index.css">
+    
+
+</head>
+<body>
+   
+    </div>
+        
+    
+</body>
+</html> 
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>#########</title>
+    <link rel="stylesheet" href="css/index.css">
+    
+
+</head>
+<body>
+    <div class="main">
+        <?php include("includes/header.php")
+        ?>
+        <div class="content">
+                <h1>########</h1>
+                <p class="par"></p>
+
+            </div>
+    </div>
+        
+    <?php include("includes/footer.php")?>
+</body>
+</html> 
